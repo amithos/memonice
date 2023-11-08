36069c60c4e2552a22950600425368f87f16b4f0
well-darma <kiu.neniu@gmail.com>
2023-11-09 00:39:34 +0200

    testing the gh-page ip

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import {useRef} from 'react';
 import logo from './memonice_3.jpeg';
 import './App.css';
 
 function App() {
+
+  const input = useRef<HTMLInputElement | null>(null);
+
+  const sendRequest = () => {
+    if (!input.current || !input.current.value) {
+      console.error("Cannot send request: ", input);
+      return;
+    }
+
+    fetch(`https://memonice-service.onrender.com/log?m=${input.current.value}`)
+      .then((response) => console.log(response))
+      .catch(e => console.error(e));
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
+      <label>
+          <span>Test message request: </span>
+          <input type="text" ref={input}/>
+        </label>
+        <button onClick={sendRequest}>send</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {"MEMONICE".split("").map((letter) => <span className='titler'>{letter}</span>)}
